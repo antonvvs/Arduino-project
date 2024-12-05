340e567a369e686dc3fdbc58db5a7b034b42cb6c
antonvvs <99277478+antonvvs@users.noreply.github.com>
2024-12-05 09:28:32 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,53 +1,35 @@
-
-/***********************************************************************
- * 
- * The I2C (TWI) bus scanner tests all addresses and detects devices
- * that are connected to the SDA and SCL signals.
- * 
- * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+/* 
+ * Read values from I2C (TWI) temperature/humidity sensor and send
+ * them to OLED screen.
+ * (c) 2023-2024 Tomas Fryza, MIT license
  *
- * Copyright (c) 2023 Tomas Fryza
- * Dept. of Radio Electronics, Brno University of Technology, Czechia
- * This work is licensed under the terms of the MIT license.
- * 
- **********************************************************************/
+ * Developed using PlatformIO and AVR 8-bit Toolchain 3.6.2.
+ * Tested on Arduino Uno board and ATmega328P, 16 MHz.
+ */
 
-
-/* Defines -----------------------------------------------------------*/
-#ifndef F_CPU
-# define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
-#endif
-
-
-/* Includes ----------------------------------------------------------*/
+// -- Includes -------------------------------------------------------
 #include <avr/io.h>         // AVR device-specific IO definitions
 #include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
 #include "timer.h"          // Timer library for AVR-GCC
 #include <twi.h>            // I2C/TWI library for AVR-GCC
+#include <oled.h>
+#include <stdio.h>          // C library for `sprintf`
 #include <uart.h>           // Peter Fleury's UART library
 #include <stdlib.h>         // C library. Needed for number conversions
-#include <oled.h>
 
-/* Global variables --------------------------------------------------*/
+// -- Defines --------------------------------------------------------
+#define DHT_ADR 0x5c
+#define DHT_HUM_MEM 0
+#define DHT_TEMP_MEM 2
 
+#ifndef F_CPU
+# define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
+#endif
 
-/* Function definitions ----------------------------------------------*/
-/**********************************************************************
- * Function: Main function where the program execution begins
- * Purpose:  Call function to test all I2C (TWI) combinations and send
- *           detected devices to UART.
- * Returns:  none
- * 
- * Some known devices:
- *     0x3c - OLED display
- *     0x57 - EEPROM
- *     0x5c - Temp+Humid
- *     0x68 - RTC
- *     0x68 - GY521
- *     0x76 - BME280
- *
- **********************************************************************/
-/* Global variables --------------------------------------------------*/
+// -- Global variables -----------------------------------------------
+volatile uint8_t flag_update_oled = 0;
+volatile uint8_t dht12_values[5];
+
 // Declaration of "dht12" variable with structure "DHT_values_structure"
 struct DHT_values_structure {
    uint8_t hum_int;
@@ -60,36 +42,52 @@ struct DHT_values_structure {
 // Flag for printing new data from sensor
 volatile uint8_t new_sensor_data = 0;
 
-
 // Slave and internal addresses of temperature/humidity sensor DHT12
 #define SENSOR_ADR 0x5c
 #define SENSOR_HUM_MEM 0
 #define SENSOR_TEMP_MEM 2
 #define SENSOR_CHECKSUM 4
 
-/* Function prototypes -----------------------------------------------*/
-void adc_init(void);
-uint16_t adc_read(uint8_t channel);
-void open_window(void);
+// -- Function definitions -------------------------------------------
+void oled_setup(void)
+{
+    oled_init(OLED_DISP_ON);
+    oled_clrscr();
 
+    oled_charMode(DOUBLESIZE);
+    oled_puts("KYTKA DATA");
 
-/**********************************************************************
-* Function: Initialize ADC
-* Purpose:  Set up the ADC to read analog values.
-**********************************************************************/
+    oled_charMode(NORMALSIZE);
+
+    oled_gotoxy(0, 2);
+    oled_puts("Svetlo: ");
+    oled_gotoxy(0, 3);
+    oled_puts("Vlh. pudy: ");
+    oled_gotoxy(0, 4);
+    oled_puts("Tep. vzduch: ");
+    oled_gotoxy(0, 5);
+    oled_puts("Vlh. vzduch: ");
+    oled_gotoxy(0, 6);
+    oled_puts("STAV: ");
+
+    // Copy buffer to display RAM
+    oled_display();
+}
+
+void timer1_init(void)
+{
+    TIM1_overflow_1s();
+    TIM1_overflow_interrupt_enable();
+}
+
 void adc_init(void)
 {
     // Set the reference voltage to AVcc
     ADMUX |= (1 << REFS0);
-    // Enable the ADC and set the prescaler to 128 (16MHz/128 = 125kHz)
+    // Enable the ADC and set the prescaler
     ADCSRA |= (1 << ADEN) | (7 << ADPS0);
 }
 
-/**********************************************************************
-* Function: Read ADC value
-* Purpose:  Read the analog value from a given ADC channel.
-* Returns:  10-bit ADC value
-**********************************************************************/
 uint16_t adc_read(uint8_t channel)
 {
     // Select the ADC channel
@@ -101,33 +99,20 @@ uint16_t adc_read(uint8_t channel)
     // Return the ADC value
     return ADC;
 }
-
-/**********************************************************************
-* Function: Open window
-* Purpose:  Control the mechanism to open the window.
-**********************************************************************/
+// příprava na servo pro otevření okna
 void open_window(void) {
-    // Code to open the window, for example, setting a pin high
-    // Adjust the code based on your hardware setup
+    
     PORTB |= (1 << PB0);
 }
-// -- Function definitions -------------------------------------------
-
-/* Function definitions ----------------------------------------------*/
-/**********************************************************************
-* Function: Main function where the program execution begins
-* Purpose:  Wait for new data from the sensor and sent them to UART.
-* Returns:  none
-**********************************************************************/
-
-
 
 int main(void)
 {
-    char string[10];  // String for converting numbers by itoa()
+    char oled_msg[20];
+    
     uint16_t light_level;  // Variable for light level
     uint16_t moisture_level;  // Variable for soil moisture level
     const char *moisture_status;  // Variable for soil moisture status
+
     // Initialize ADC
     adc_init();
 
@@ -137,122 +122,123 @@ int main(void)
     // UART
     uart_init(UART_BAUD_SELECT(115200, F_CPU));
 
-    sei();  // Needed for UART
-
-    // Test if sensor is ready
-    if (twi_test_address(SENSOR_ADR) == 0)
-        uart_puts("I2C sensor detected\r\n");
-    else {
-        uart_puts("[ERROR] I2C device not detected\r\n");
-        while (1);
-    }
+    // OLED setup
+    oled_setup();
 
     // Timer1
-    TIM1_overflow_1s();
-    TIM1_overflow_interrupt_enable();
+    timer1_init();
 
     sei();
 
+    
+
     // Infinite loop
-    while (1) {
-        if (new_sensor_data == 1) {
+    while (1)
+    {
+        if (flag_update_oled == 1)
+        {
             // Read photoresistor value
             light_level = adc_read(0);  // Read the light level from ADC channel 0
-
-            // detekce svetelneho mnoztvi
-            if (light_level > 482) {  // Adjust threshold as needed
-                uart_puts("Den\r\n");
-            } else {
-                uart_puts("Noc\r\n");
-            }
-             // Read soil moisture level
-            moisture_level = adc_read(1);  // Read the soil moisture level from ADC channel 1
-
-            // pudni senzor nastaveni
-            if (moisture_level > 500) {
-                moisture_status = "senzor mimo půdu";
-            } else if (moisture_level > 260) {
-                moisture_status = "suché";
-            } else {
-                moisture_status = "zalito";
-            }
-
-            // otevření okna procenta
-            if (moisture_level >= 300) {
-                uart_puts("zalit\n");
-                open_window();
-            }
-
             
-            uart_puts("vhlkost půdy: ");
-            uart_puts(moisture_status);
-            uart_puts("\r\n");
+            // Light level detection (highet value means day)
+            oled_gotoxy(14, 2);
+            if (light_level > 700) {  
+                sprintf(oled_msg, "Den ");
+            } else {
+                sprintf(oled_msg, "Noc");
+            }
+            oled_puts(oled_msg);
+            
+    
 
+            // Read soil moisture level
+            moisture_level = adc_read(1);  
 
-            itoa(dht12.temp_int, string, 10);
-            uart_puts("teplota vzduchu: ");
-            uart_puts(string);
-            uart_puts(".");
-            itoa(dht12.temp_dec, string, 10);
-            uart_puts(string);
-            uart_puts(" °C\r\n");
+            // Soil moisture status
+            oled_gotoxy(14, 3);
+            if (moisture_level > 500) { 
+                moisture_status = "Out ";
+            } else if (moisture_level > 260) {
+                moisture_status = "Dry";
+            } else {
+                moisture_status = "Wet";
+            }
+            oled_puts(moisture_status);
 
-            itoa(dht12.hum_int, string, 10);
-            uart_puts("vhlkost vzduchu: ");
-            uart_puts(string);
-            uart_puts(".");
-            itoa(dht12.hum_dec, string, 10);
-            uart_puts(string);
-            uart_puts(" %\r\n");
-            uart_puts("\r\n");
+            // Air temperature
+            oled_gotoxy(14, 4);
+            sprintf(oled_msg, "%u.%u C", dht12.temp_int, dht12.temp_dec);
+            oled_puts(oled_msg);
 
-            new_sensor_data = 0;
+            // Air humidity
+            oled_gotoxy(14, 5);
+            sprintf(oled_msg, "%u.%u %%", dht12.hum_int, dht12.hum_dec);
+            oled_puts(oled_msg);
+
+            // Watering status
+            oled_gotoxy(14, 6);
+            if (moisture_level >= 300) {
+                sprintf(oled_msg, "DRY");
+                open_window();
+            } else {
+                sprintf(oled_msg, "WET");
+            }
+            oled_puts(oled_msg);
+
+            // Update OLED display
+            oled_display();
+
+            // Reset flag
+            flag_update_oled = 0;
         }
     }
 
-    // Will never reach this
+    
     return 0;
 }
 
 
-/**********************************************************************
-* Function: Timer/Counter1 overflow interrupt
-* Purpose:  Read temperature and humidity from DHT12, SLA = 0x5c.
-**********************************************************************/
 ISR(TIMER1_OVF_vect)
 {
-    // Test ACK from sensor
-    twi_start();
-    if (twi_write((SENSOR_ADR<<1) | TWI_WRITE) == 0) {
-        // Set internal memory location
-        twi_write(SENSOR_TEMP_MEM);
-        twi_stop();
-        // Read data from internal memory
+    static uint8_t n_ovfs = 0;
+
+    n_ovfs++;
+    // Read the data every 2 secs
+    if (n_ovfs >= 2)
+    {
+        n_ovfs = 0;
+        twi_readfrom_mem_into(DHT_ADR, DHT_HUM_MEM, dht12_values, 5);
+        flag_update_oled = 1;
+
+        // Test ACK from sensor
         twi_start();
-        twi_write((SENSOR_ADR<<1) | TWI_READ);
-        dht12.temp_int = twi_read(TWI_ACK);
-        dht12.temp_dec = twi_read(TWI_NACK);
+        if (twi_write((SENSOR_ADR<<1) | TWI_WRITE) == 0) {
+            // Set internal memory location
+            twi_write(SENSOR_TEMP_MEM);
+            twi_stop();
+            // Read data from internal memory
+            twi_start();
+            twi_write((SENSOR_ADR<<1) | TWI_READ);
+            dht12.temp_int = twi_read(TWI_ACK);
+            dht12.temp_dec = twi_read(TWI_NACK);
 
-        new_sensor_data = 1;
-    }
-    twi_stop();
-
-
-    twi_start();
-    if (twi_write((SENSOR_ADR<<1) | TWI_WRITE) == 0) {
-        // Set internal memory location
-        twi_write(SENSOR_HUM_MEM);
+            new_sensor_data = 1;
+        }
         twi_stop();
-        // Read data from internal memory
-        twi_start();
-        twi_write((SENSOR_ADR<<1) | TWI_READ);
-        dht12.hum_int = twi_read(TWI_ACK);
-        dht12.hum_dec = twi_read(TWI_NACK);
 
-        new_sensor_data = 1;
+        twi_start();
+        if (twi_write((SENSOR_ADR<<1) | TWI_WRITE) == 0) {
+            // Set internal memory location
+            twi_write(SENSOR_HUM_MEM);
+            twi_stop();
+            // Read data from internal memory
+            twi_start();
+            twi_write((SENSOR_ADR<<1) | TWI_READ);
+            dht12.hum_int = twi_read(TWI_ACK);
+            dht12.hum_dec = twi_read(TWI_NACK);
+
+            new_sensor_data = 1;
+        }
+        twi_stop();
     }
-    twi_stop();
 }
-
-
-/* Interrupt service routines ----------------------------------------*/
